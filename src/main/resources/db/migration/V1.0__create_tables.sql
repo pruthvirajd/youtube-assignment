232d632fece6195af99b6fdb9367391fcadbf37c
Pruthviraj Deshmukh <pruthvirajd@Pruthvirajs-MacBook-Pro.local>
2022-09-03 18:42:13 +0530

    Configured flyway for initial setup

---
+++ src/main/resources/db/migration/V1.0__create_tables.sql
@@ -0,0 +1,34 @@
+CREATE TABLE [tChannel](
+ChannelID int IDENTITY(0,1) PRIMARY KEY,
+YoutubeChannelID varchar(255) NOT NULL,
+ChannelTitle varchar(255) NOT NULL
+)
+GO
+
+CREATE TABLE [tVideo](
+VideoID int IDENTITY(0,1) PRIMARY KEY,
+YouTubeVideoID varchar(255) NOT NULL,
+VideoTitle varchar(255) NOT NULL,
+VideoDescription varchar(1000),
+PublishedDate DATETIME NOT NULL,
+ChannelID int NOT NULL
+CONSTRAINT FK_Channel FOREIGN KEY (ChannelID) REFERENCES tChannel(ChannelID)
+)
+GO
+
+CREATE TABLE [tThumbnail](
+ThumbnailID int IDENTITY(0,1) PRIMARY KEY,
+DefaultURL varchar(255),
+MediumURL varchar(255),
+HighURL varchar(255)
+)
+GO
+
+CREATE TABLE [tVideoToThumbnailMap](
+VideoToThumbnailMapID int IDENTITY(0,1) PRIMARY KEY,
+VideoID int,
+ThumbnailID int
+CONSTRAINT FK_Video FOREIGN KEY (VideoID) REFERENCES tVideo(VideoID),
+CONSTRAINT FK_Thumbnail FOREIGN KEY (ThumbnailID) REFERENCES tThumbnail(ThumbnailID)
+)
+GO
