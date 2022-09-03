71c88182f9654a7b27bbaa0da4531ef6ddb8bfa9
Pruthviraj Deshmukh <pruthvirajd@Pruthvirajs-MacBook-Pro.local>
2022-09-03 14:17:18 +0530

    Initial commit for Repo setup

---
+++ src/main/java/com/pruthviraj/youtubeassignment/YoutubeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.pruthviraj.youtubeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruthviraj/youtubeassignment/YoutubeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruthviraj.youtubeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
